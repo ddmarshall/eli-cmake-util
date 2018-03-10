3acea0bd3ee9d99f412899a5e3576ca718257420
ddmarshall <ddmarshall@gmail.com>
2018-03-09 17:15:11 -0800

    Created CMake function for debugging

---
+++ print_all_variables.cmake
@@ -0,0 +1,26 @@
+################################################################################
+# Copyright (c) 2015-2018 David D. Marshall <ddmarsha@calpoly.edu>
+#
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# See LICENSE.md file in the project root for full license information.
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#    David D. Marshall - initial code and implementation
+################################################################################
+#
+# - Prints all of the variables in scope at time of call. Based on this
+#   https://stackoverflow.com/a/9328525 StackOverflow answer.
+#
+
+function(PRINT_ALL_VARIABLES)
+  get_cmake_property(_variableNames VARIABLES)
+  foreach (_variableName ${_variableNames})
+    message(STATUS "${_variableName}=${${_variableName}}")
+  endforeach()
+endfunction()
+
