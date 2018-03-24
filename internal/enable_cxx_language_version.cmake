6513321db68a72f8294cb8a277bf151ba0d70e77
ddmarshall <ddmarshall@gmail.com>
2018-03-23 23:10:10 -0700

    Modified for line widths.

--- internal/enable_cxx_language_version.cmake
+++ internal/enable_cxx_language_version.cmake
@@ -47,62 +47,72 @@ function(ENABLE_CXX_LANGUAGE_VERSION flag_name_ version_number_)
   # flag for C++98
   if(version_number_ EQUAL 1998)
     set(FLAG_STRING "-std=c++98") # g++, clang++
-    check_cxx_compiler_flag(${FLAG_STRING} COMPILER_SUPPORT_CXX98_LANGUAGE_SETTING)
+    check_cxx_compiler_flag(${FLAG_STRING} 
+                            COMPILER_SUPPORT_CXX98_LANGUAGE_SETTING)
     if (COMPILER_SUPPORT_CXX98_LANGUAGE_SETTING)
       set(${flag_name_} ${FLAG_STRING} PARENT_SCOPE)
     endif()
   # flag for C++03
   elseif(version_number_ EQUAL 2003)
     set(FLAG_STRING "-std=c++03") # g++, clang++
-    check_cxx_compiler_flag(${FLAG_STRING} COMPILER_SUPPORT_CXX03_LANGUAGE_SETTING)
+    check_cxx_compiler_flag(${FLAG_STRING} 
+                            COMPILER_SUPPORT_CXX03_LANGUAGE_SETTING)
     if (COMPILER_SUPPORT_CXX03_LANGUAGE_SETTING)
       set(${flag_name_} ${FLAG_STRING} PARENT_SCOPE)
     endif()
   # flag for C++11
   elseif(version_number_ EQUAL 2011)
     set(FLAG_STRING "-std=c++11") # g++, clang++
-    check_cxx_compiler_flag(${FLAG_STRING} COMPILER_SUPPORT_CXX11_LANGUAGE_SETTING)
+    check_cxx_compiler_flag(${FLAG_STRING} 
+                            COMPILER_SUPPORT_CXX11_LANGUAGE_SETTING)
     if (COMPILER_SUPPORT_CXX11_LANGUAGE_SETTING)
       set(${flag_name_} ${FLAG_STRING} PARENT_SCOPE)
     else()
       set(FLAG_STRING "-std=c++0x") # g++, clang++
-      check_cxx_compiler_flag(${FLAG_STRING} COMPILER_SUPPORT_CXX0X_LANGUAGE_SETTING)
+      check_cxx_compiler_flag(${FLAG_STRING} 
+                            COMPILER_SUPPORT_CXX0X_LANGUAGE_SETTING)
       if (COMPILER_SUPPORT_CXX0X_LANGUAGE_SETTING)
         set(${flag_name_} ${FLAG_STRING} PARENT_SCOPE)
       endif()
     endif()
   elseif(version_number_ EQUAL 2014)
     set(FLAG_STRING "-std=c++14") # g++, clang++
-    check_cxx_compiler_flag(${FLAG_STRING} COMPILER_SUPPORT_CXX14_LANGUAGE_SETTING)
+    check_cxx_compiler_flag(${FLAG_STRING} 
+                            COMPILER_SUPPORT_CXX14_LANGUAGE_SETTING)
     if (COMPILER_SUPPORT_CXX14_LANGUAGE_SETTING)
       set(${flag_name_} ${FLAG_STRING} PARENT_SCOPE)
     else()
       set(FLAG_STRING "-std=c++1y") # g++, clang++
-      check_cxx_compiler_flag(${FLAG_STRING} COMPILER_SUPPORT_CXX1Y_LANGUAGE_SETTING)
+      check_cxx_compiler_flag(${FLAG_STRING} 
+                            COMPILER_SUPPORT_CXX1Y_LANGUAGE_SETTING)
       if (COMPILER_SUPPORT_CXX1Y_LANGUAGE_SETTING)
         set(${flag_name_} ${FLAG_STRING} PARENT_SCOPE)
       endif()
     endif()
   elseif(version_number_ EQUAL 2017)
     set(FLAG_STRING "-std=c++17") # g++, clang++
-    check_cxx_compiler_flag(${FLAG_STRING} COMPILER_SUPPORT_CXX17_LANGUAGE_SETTING)
+    check_cxx_compiler_flag(${FLAG_STRING} 
+                            COMPILER_SUPPORT_CXX17_LANGUAGE_SETTING)
     if (COMPILER_SUPPORT_CXX17_LANGUAGE_SETTING)
       set(${flag_name_} ${FLAG_STRING} PARENT_SCOPE)
     else()
       set(FLAG_STRING "-std=c++1z") # g++, clang++
-      check_cxx_compiler_flag(${FLAG_STRING} COMPILER_SUPPORT_CXX1Z_LANGUAGE_SETTING)
+      check_cxx_compiler_flag(${FLAG_STRING} 
+                            COMPILER_SUPPORT_CXX1Z_LANGUAGE_SETTING)
       if (COMPILER_SUPPORT_CXX1Z_LANGUAGE_SETTING)
         set(${flag_name_} ${FLAG_STRING} PARENT_SCOPE)
       endif()
     endif()
   elseif(version_number_ EQUAL 2020)
     set(FLAG_STRING "-std=c++20") # g++, clang++
-    check_cxx_compiler_flag(${FLAG_STRING} COMPILER_SUPPORT_CXX20_LANGUAGE_SETTING)
+    check_cxx_compiler_flag(${FLAG_STRING} 
+                            COMPILER_SUPPORT_CXX20_LANGUAGE_SETTING)
     if (COMPILER_SUPPORT_CXX20_LANGUAGE_SETTING)
       set(${flag_name_} ${FLAG_STRING} PARENT_SCOPE)
     else()
       set(FLAG_STRING "-std=c++2a") # g++, clang++
-      check_cxx_compiler_flag(${FLAG_STRING} COMPILER_SUPPORT_CXX2A_LANGUAGE_SETTING)
+      check_cxx_compiler_flag(${FLAG_STRING} 
+                            COMPILER_SUPPORT_CXX2A_LANGUAGE_SETTING)
       if (COMPILER_SUPPORT_CXX2A_LANGUAGE_SETTING)
         set(${flag_name_} ${FLAG_STRING} PARENT_SCOPE)
       endif()
